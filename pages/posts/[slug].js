9fa4173f06573f4b829618180c0bbd02ad39f52d
pedrolmr <pedroluismontesinosrivera@gmail.com>
2021-05-19 01:55:07 -0400

    we are generating paths now

--- pages/posts/[slug].js
+++ pages/posts/[slug].js
@@ -1,3 +1,39 @@
-export default function PostDetails() {
+import { createClient } from "contentful";
+
+const client = createClient({
+  space: process.env.CONTENTFUL_SPACE_ID,
+  accessToken: process.env.CONTENTFUL_ACCESS_KEY,
+});
+
+export const getStaticPaths = async () => {
+  const res = await client.getEntries({
+    content_type: "post",
+  });
+
+  const paths = res.items.map((item) => {
+    return {
+      params: { slug: item.fields.slug },
+    };
+  });
+
+  return {
+    paths,
+    fallback: false,
+  };
+};
+
+export async function getStaticProps({ params }) {
+  const res = await client.getEntries({
+    content_type: "post",
+    "fields.slug": params.slug,
+  });
+
+  return {
+    props: { post: res.items[0] },
+  };
+}
+
+export default function PostDetails({ post }) {
+  console.log(post);
   return <div>Post Details</div>;
 }
