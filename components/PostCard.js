f020cf2b83fe8b8d771f4dda94e640d3d2e74cd7
pedrolmr <pedroluismontesinosrivera@gmail.com>
2021-05-23 01:29:22 -0400

    formatted the date on the PostCard component

--- components/PostCard.js
+++ components/PostCard.js
@@ -9,7 +9,9 @@ export default function PostCard({ post }) {
         <a>
           <div className="border-2 border-red-300">
             <h3 className="text-lg font-bold ">{title}</h3>
-            <span className="text-sm font-thin color-gray-200">{date}</span>
+            <span className="text-sm font-thin color-gray-200">
+              {new Date(date).toLocaleDateString()}
+            </span>
           </div>
         </a>
       </Link>
